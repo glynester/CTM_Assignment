e25e1d86aebb84dc3492a0fe5e4075530913188c
glynester <glynester@hotmail.com>
2017-04-10 22:38:11 +0100

    New test to see that file is being opened and read.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+
+ruby '2.3.1'
+
+group :development, :test do
+  gem "rspec"
+end
+
+
+# Don't think this is required!!!
--- spec/words_test_suite_spec.rb
+++ spec/words_test_suite_spec.rb
@@ -1,19 +1,33 @@
 require_relative "../word_count"
 
+
+
 describe Words do
   context "Raising errors" do
-    it 'throws an error if the file does not exist' do
+    it 'Throws an error if the file does not exist' do
       expect { Words.new("file_not_there.txt") }.to raise_error("There is no file called 'file_not_there.txt' at this location.")
     end
-    it 'throws an error if the file is not given in quotes' do
+    it 'Throws an error if the file is not given in quotes' do
       expect { Words.new(no_quotes.txt) }.to raise_error(NameError)
     end
   end
 
-  context "Screen display" do
+  context "Screen display and prompts" do
     subject {described_class.new("test.txt")}
     it "Displays the correct screen information" do
       expect { subject.show_info }.to output(/NOTE: You can run this program by /).to_stdout
+    end
+    subject {described_class.new}
+    it "Prompts for input if no file is provided initially" do
+      expect { subject.show_info }.to output(/Paste a .txt file into the same folder /).to_stdout
+    end
+  end
+
+  before    { File.write('testfile.txt', "Contents of test file") }
+  context 'Check file content' do
+    subject {described_class.new}
+    it "Should open the file provided and read the contents" do
+      expect(subject.open_file('testfile.txt')).to eq("Contents of test file")
     end
   end
 
