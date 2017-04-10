f763fbb7896f14aa3475bc4cd6c286ced7c9ffaf
glynester <glynester@hotmail.com>
2017-04-10 17:17:13 +0100

    Changed to class structure and have 1 passing rspec test.

---
+++ spec/words_test_suite_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../word_count"
+
+describe Words do
+  context "Raising errors" do
+    it 'throws an error if the file does not exist' do
+      expect { Words.new("file_not_there.txt") }.to raise_error("There is no file called 'file_not_there.txt' at this location.")
+    end
+    it 'throws an error if the file is not given in quotes' do
+      expect { Words.new(no_quotes.txt) }.to raise_error("There is no file called 'file_not_there.txt' at this location.")
+    end
+  end
+
+end
